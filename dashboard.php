764c8fa08507116ba6d40032817e1c9aa67cc69b
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-01-08 07:20:26 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -629,41 +629,12 @@ if($_GET['dev'] == 'yes'){
                 </ul>
             </div>
         </div>
+
         <div id="main">
             <div class="container-fluid">
                 <div class="page-header">
                     <div class="pull-left">
                         <h1>Site with footer</h1>
-                    </div>
-                    <div class="pull-right">
-                        <ul class="minitiles">
-                            <li class='grey'>
-                                <a href="#">
-                                    <i class="fa fa-cogs"></i>
-                                </a>
-                            </li>
-                            <li class='lightgrey'>
-                                <a href="#">
-                                    <i class="fa fa-globe"></i>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul class="stats">
-                            <li class='satgreen'>
-                                <i class="fa fa-money"></i>
-                                <div class="details">
-                                    <span class="big">$324,12</span>
-                                    <span>Balance</span>
-                                </div>
-                            </li>
-                            <li class='lightred'>
-                                <i class="fa fa-calendar"></i>
-                                <div class="details">
-                                    <span class="big">February 22, 2013</span>
-                                    <span>Wednesday, 13:56</span>
-                                </div>
-                            </li>
-                        </ul>
                     </div>
                 </div>
                 <div class="breadcrumbs">
@@ -692,11 +663,11 @@ if($_GET['dev'] == 'yes'){
                             <div class="box-title">
                                 <h3>
                                     <i class="fa fa-bars"></i>
-                                    Basic Widget
+                                    Sample Header
                                 </h3>
                             </div>
                             <div class="box-content">
-                                Content
+                                Sample Content
                             </div>
                         </div>
                     </div>
