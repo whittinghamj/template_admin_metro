57aea09f5ced4d490dc1a28a4ecabf025b9f6e24
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-01-08 09:26:39 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -270,7 +270,149 @@ if($_GET['dev'] == 'yes'){
                                     </h3>
                                 </div>
                                 <div class="box-content">
-                                    Sender Numbers Content
+                                    <table class="table table-hover table-nomargin dataTable table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Rendering engine</th>
+                                                <th>Browser</th>
+                                                <th class='hidden-350'>Platform(s)</th>
+                                                <th class='hidden-1024'>Engine version</th>
+                                                <th class='hidden-480'>CSS grade</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Trident</td>
+                                                <td>
+                                                    Internet Explorer 4.0
+                                                </td>
+                                                <td class='hidden-350'>Win 95+</td>
+                                                <td class='hidden-1024'>4</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Presto</td>
+                                                <td>Nokia N800</td>
+                                                <td class='hidden-350'>N800</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>A</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>NetFront 3.4</td>
+                                                <td class='hidden-350'>Embedded devices</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>A</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>Dillo 0.8</td>
+                                                <td class='hidden-350'>Embedded devices</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>Links</td>
+                                                <td class='hidden-350'>Text only</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>Lynx</td>
+                                                <td class='hidden-350'>Text only</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>IE Mobile</td>
+                                                <td class='hidden-350'>Windows Mobile 6</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>C</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>PSP browser</td>
+                                                <td class='hidden-350'>PSP</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>C</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Other browsers</td>
+                                                <td>All others</td>
+                                                <td class='hidden-350'>-</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>U</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Trident</td>
+                                                <td>
+                                                    Internet Explorer 4.0
+                                                </td>
+                                                <td class='hidden-350'>Win 95+</td>
+                                                <td class='hidden-1024'>4</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Presto</td>
+                                                <td>Nokia N800</td>
+                                                <td class='hidden-350'>N800</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>A</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>NetFront 3.4</td>
+                                                <td class='hidden-350'>Embedded devices</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>A</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>Dillo 0.8</td>
+                                                <td class='hidden-350'>Embedded devices</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>Links</td>
+                                                <td class='hidden-350'>Text only</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>Lynx</td>
+                                                <td class='hidden-350'>Text only</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>X</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>IE Mobile</td>
+                                                <td class='hidden-350'>Windows Mobile 6</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>C</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misc</td>
+                                                <td>PSP browser</td>
+                                                <td class='hidden-350'>PSP</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>C</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Other browsers</td>
+                                                <td>All others</td>
+                                                <td class='hidden-350'>-</td>
+                                                <td class='hidden-1024'>-</td>
+                                                <td class='hidden-480'>U</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
