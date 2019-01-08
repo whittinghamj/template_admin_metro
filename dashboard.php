fce13e9b968c478cab621f512ea134def8eca301
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-01-08 07:11:41 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -49,44 +49,55 @@ if($_GET['dev'] == 'yes'){
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
+
     <!-- jQuery UI -->
     <link rel="stylesheet" href="css/plugins/jquery-ui/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="css/plugins/jquery-ui/smoothness/jquery.ui.theme.css">
+
     <!-- Theme CSS -->
     <link rel="stylesheet" href="css/style.css">
+
     <!-- Color CSS -->
     <link rel="stylesheet" href="css/themes.css">
-
 
     <!-- jQuery -->
     <script src="js/jquery.min.js"></script>
 
     <!-- Nice Scroll -->
     <script src="js/plugins/nicescroll/jquery.nicescroll.min.js"></script>
+
     <!-- imagesLoaded -->
     <script src="js/plugins/imagesLoaded/jquery.imagesloaded.min.js"></script>
+
     <!-- jQuery UI -->
     <script src="js/plugins/jquery-ui/jquery.ui.core.min.js"></script>
     <script src="js/plugins/jquery-ui/jquery.ui.widget.min.js"></script>
     <script src="js/plugins/jquery-ui/jquery.ui.mouse.min.js"></script>
     <script src="js/plugins/jquery-ui/jquery.ui.resizable.min.js"></script>
     <script src="js/plugins/jquery-ui/jquery.ui.sortable.min.js"></script>
+
     <!-- slimScroll -->
     <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
     <!-- Bootstrap -->
     <script src="js/bootstrap.min.js"></script>
+
     <!-- Bootbox -->
     <script src="js/plugins/bootbox/jquery.bootbox.js"></script>
+
     <!-- Bootbox -->
     <script src="js/plugins/form/jquery.form.min.js"></script>
+
     <!-- Validation -->
     <script src="js/plugins/validation/jquery.validate.min.js"></script>
     <script src="js/plugins/validation/additional-methods.min.js"></script>
 
     <!-- Theme framework -->
     <script src="js/eakroko.min.js"></script>
+
     <!-- Theme scripts -->
     <script src="js/application.min.js"></script>
+
     <!-- Just for demonstration -->
     <script src="js/demonstration.min.js"></script>
 
@@ -97,10 +108,11 @@ if($_GET['dev'] == 'yes'){
                 $('input, textarea').placeholder();
             });
         </script>
-        <![endif]-->
+    <![endif]-->
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="img/favicon.ico" />
+
     <!-- Apple devices Homescreen icon -->
     <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.png" />
 
@@ -744,11 +756,9 @@ if($_GET['dev'] == 'yes'){
     </div>
     <div id="footer">
         <p>
-            FLAT - Responsive Admin Template
+            <?php echo $site['title']; ?> &copy;
             <span class="font-grey-4">|</span>
-            <a href="#">Contact</a>
-            <span class="font-grey-4">|</span>
-            <a href="#">Imprint</a>
+            <a href="https://genexnetworks.net/">Written by Genex Networks LLC</a>
         </p>
         <a href="#" class="gototop">
             <i class="fa fa-arrow-up"></i>
