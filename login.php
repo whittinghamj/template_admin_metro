bb155f6c9ac2893fa59c37ba9dd6343bcd8d6262
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-01-08 03:31:30 +0000

    no message

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <?php
 
 // if($_GET['dev'] == 'yes'){
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
-  ini_set('error_reporting', E_ALL);
+  	error_reporting(E_ALL);
+  	ini_set('error_reporting', E_ALL);
+  	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
 // }
 
 include("inc/db.php");
