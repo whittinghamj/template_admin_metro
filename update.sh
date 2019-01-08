de3b6d9e42c45b0443c15fe76cc1ca2742c08695
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-01-08 01:33:35 +0000

    added update.sh script for git pull

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home2/jamie/public_html/projects/template_admin_metro  && git --git-dir=/home2/jamie/public_html/projects/template_admin_metro/.git pull origin master
+
+# crontab /mcp_cluster/crontab.txt
