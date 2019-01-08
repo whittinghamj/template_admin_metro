937da4ddfd1c9d50b86f62c3f71a0dd9f90941cf
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-01-08 04:08:30 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,37 @@
+<?php
+if($_GET['dev'] == 'yes'){
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('error_reporting', E_ALL);
+}
+
+include('inc/db.php');
+include('inc/sessions.php');
+$sess = new SessionManager();
+session_start();
+
+// start timer for page loaded var
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$start = $time;
+
+// includes
+include('inc/global_vars.php');
+include('inc/functions.php');
+
+// check is account->id is set, if not then assume user is not logged in correctly and redirect to login page
+if(empty($_SESSION['account']['id'])){
+  status_message('danger', 'Login Session Timeout');
+  go($site['url'].'/index?c=session_timeout');
+}
+
+// get account details for logged in user
+$account_details = account_details($_SESSION['account']['id']);
+
+dubug($account_details);
+die();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
